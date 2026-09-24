a5f88dad9e578c662af81e945612819738ee050d
NextSap <nextsap@outlook.com>
2026-09-24 20:28:32 +0200

    Add a backend call (HelloWorld user story)

--- JS/appjs.js
+++ JS/appjs.js
@@ -6,3 +6,19 @@ function updateClock()
 
 updateClock();
 setInterval(updateClock, 1000);
+
+async function getHelloWorld() {
+    const url = "http://localhost:8080/api/test/2";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            console.error("Error from backend")
+            return;
+        }
+
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
